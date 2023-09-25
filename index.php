5ec58026563bd86f1ee66984e2da7804a7bedd21
matteo <matteoackermann456@gmail.com>
2023-09-25 14:32:30 +0200

    changement vues->views

--- index.php
+++ index.php
@@ -2,4 +2,4 @@
 $page_description = "description de la page";
 $page_title="titre de la page";
 $page_content="";
-require_once ("views/template.php");
+require_once ("views/common/template.php");
---
+++ views/common/template.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="<?=$page_description;?>">
+    <title><?=$page_title;?></title>
+
+</head>
+<body>
+<?php require_once ("views/acceuil.php");?>
+<?=$page_content;?>
+
+</body>
+
+
+
+
+</html>
