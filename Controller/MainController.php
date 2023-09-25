bf31cbde95d18c59d5944a5a0b827d30a5c7bb48
Nukey <vincenthoar@hotmail.fr>
2023-09-25 14:12:14 +0200

    creation de controller

---
+++ Controller/MainController.php
@@ -0,0 +1,37 @@
+<?php
+
+class MainController{
+    public function accueil(){
+        $page_description = "";
+        $page_title = "";
+        ob_start();
+        require_once("./Vues/accueil.php");
+        $page_content = ob_get_clean();
+
+    }
+    
+    public function authentification(){
+        $page_description = "";
+        $page_title = "";
+        ob_start();
+        require_once("./Vues/authentification.php");
+        $page_content = ob_get_clean();
+    }
+    
+    public function inscription(){
+        $page_description = "";
+        $page_title = "";
+        $page_content = 
+        ob_start();
+        require_once("./Vues/inscription.php");
+        $page_content = ob_get_clean();
+    }
+    
+    public function profile(){
+        $page_description = "";
+        $page_title = "";
+        ob_start();
+        require_once("./Vues/profile.php");
+        $page_content = ob_get_clean();
+    }
+}
