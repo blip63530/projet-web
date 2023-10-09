b85a340d99d7eb4bb77d9b4f4bcdcd25245aa839
matteo <matteoackermann456@gmail.com>
2023-10-09 14:14:57 +0200

    ajout floppy ca marche

--- views/flappy.php
+++ views/flappy.php
@@ -4,8 +4,8 @@
     <title>Flappy Bird using JS | by learnWD</title>
   </head>
   <body>
-  <div class="page_content">
-   <h3>flappyBird by LearnWD</h3>
+  <div class="page_flappy">
+
    
    <canvas id="canvas" width="288" height="512"></canvas>
    
