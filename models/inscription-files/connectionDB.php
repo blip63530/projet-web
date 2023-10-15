44c7647e82989ba0694a80b492722ffd51cf3468
matteo <matteoackermann456@gmail.com>
2023-10-15 19:43:56 +0200

    ajout des scores

--- models/inscription-files/connectionDB.php
+++ models/inscription-files/connectionDB.php
@@ -1,7 +1,7 @@
 <?php
 // Fichier de configuration contenant les informations de connexion
-include 'loginDB.php';
-
+//include 'loginDB.php';
+require_once '../../loginDB.php';
 function connectDB() {
     global $servername, $username, $password, $dbname;
 
@@ -11,7 +11,7 @@ function connectDB() {
 
         // Configuration de PDO pour générer des exceptions en cas d'erreur
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        echo "Connexion à la base de données réussie. :) :) :) :) :) :) :)";
+        //echo "Connexion à la base de données réussie. :) :) :) :) :) :) :)";
         return $pdo;
     } catch (PDOException $e) {
         // En cas d'erreur de connexion, affichez le message d'erreur
@@ -53,13 +53,36 @@ function connection($nom, $pw)
     $pdo=null;
 }
 
-
-
 // Appel de la fonction de connexion
 $pdo = connectDB();
 
 // Vous pouvez maintenant utiliser $pdo pour exécuter des requêtes SQL
 // Par exemple : $pdo->query("SELECT * FROM ma_table");
+
+
+
+//coonection mysqli
+$conn=mysqli_connect($servername,$username,$password,$dbname);
+// requete des score floppy
+$result = mysqli_query($conn,'SELECT Comptes.Name AS Pseudo, ScoreNb FROM Scores
+INNER JOIN Comptes ON Scores.UID=Comptes.UID
+INNER JOIN Jeu ON Scores.IDGAME = Jeu.IDGAME
+WHERE NomJeu = "flappy" ORDER BY ScoreNb DESC LIMIT 10;');
+
+$data = array();
+while ($row = mysqli_fetch_assoc($result)){
+    $data[] = $row;
+}
+// returning in form of json
+echo json_encode($data);
+
+
+
+
+
+
+
+
 
 // Fermez la connexion lorsque vous avez terminé
 $pdo = null;
--- views/flappy.php
+++ views/flappy.php
@@ -4,12 +4,68 @@
     <title>Flappy Bird using JS | by learnWD</title>
   </head>
   <body>
+  <div class="jeux_score">
+
+
   <div class="page_flappy">
 
    
    <canvas id="canvas" width="288" height="512"></canvas>
    
-   <script src="../scripts/flappyBird.js"></script>
+ <!--<script src="../scripts/flappyBird.js"></script>-->
   </div>
+  <div class="scoress">
+    <p id="pscorres">scores</p>
+      <table>
+      <tbody id="tabscores">
+
+      </tbody>
+      </table>
+
+
+  </div>
+  </div>
+
+  <script>
+      document.addEventListener("DOMContentLoaded", () => {
+          //call ajax
+          var ajax = new XMLHttpRequest();
+          var method = "GET";
+          var url = "../models/inscription-files/connectionDB.php";
+          var asynchronous = true;
+          ajax.open(method, url, asynchronous);
+          //sending ajax request
+          ajax.send();
+
+          //receving response from data.php
+          ajax.onreadystatechange = function () {
+
+              if (this.readyState == 4 && this.status == 200) {
+
+                  var data = JSON.parse(this.responseText);//converting json to array
+                  console.log(data);
+                  var html = "";
+                  for (var a = 0; a < data.length; a++) {
+                      var score = data[a].ScoreNb;
+                      var nom = data[a].Pseudo;
+
+                      //happending html
+                      html += "<tr>";
+                      html += "<td>" + score + "</td>";
+                      html += "<td>" + nom + "</td>";
+                      html += "</tr>";
+
+
+                  }
+                  //replacing tbody
+                  document.getElementById("tabscores").innerHTML = html;
+              }
+          }
+      });
+
+
+
+  </script>
   </body>
+
 </html>
