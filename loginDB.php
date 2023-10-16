fc33423a1527f19c21e6aa25b95c2b90c67fac14
matteo <matteoackermann456@gmail.com>
2023-10-16 21:25:26 +0200

    test fix

--- loginDB.php
+++ loginDB.php
@@ -3,4 +3,5 @@ $servername = "82.65.68.131";
 $username = "prod";
 $password = "joTTjXTIJ3CI2ade";
 $dbname = "projet-web";
+$db_port        = '3306';
 ?>
--- models/inscription-files/scoresflappy.php
+++ models/inscription-files/scoresflappy.php
@@ -5,7 +5,7 @@ require_once '../../loginDB.php';
 
 
 //coonection mysqli
-$conn=mysqli_connect($servername,$username,$password,$dbname);
+$conn=mysqli_connect($servername,$username,$password,$dbname,$db_port);
 
 echo $conn->host_info . "\n";
 // requete des score floppy
