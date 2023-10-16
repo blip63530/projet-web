beb8d156a4e9b44553d56c5f80671e1fd1fd7a72
matteo <matteoackermann456@gmail.com>
2023-10-16 20:16:29 +0200

    test fix

--- models/inscription-files/scoresflappy.php
+++ models/inscription-files/scoresflappy.php
@@ -3,7 +3,7 @@
 //include 'loginDB.php';
 require_once '../../loginDB.php';
 
-echo $servername;
+
 //coonection mysqli
 $conn=mysqli_connect($servername,$username,$password,$dbname);
 
@@ -12,12 +12,12 @@ $result = mysqli_query($conn,'SELECT Comptes.Name AS Pseudo, ScoreNb FROM Scores
 INNER JOIN Comptes ON Scores.UID=Comptes.UID
 INNER JOIN Jeu ON Scores.IDGAME = Jeu.IDGAME
 WHERE NomJeu = "flappy" ORDER BY ScoreNb DESC LIMIT 10;');
-//echo('3');
+
 $data = array();
 while ($row = mysqli_fetch_assoc($result)){
     $data[] = $row;
 }
-//echo('4');
+echo$data;
 // returning in form of json
 echo json_encode($data);
 //echo('5');
