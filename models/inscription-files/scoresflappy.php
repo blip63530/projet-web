be2f274347642144321bec04f3a1e03756c60a24
matteo <matteoackermann456@gmail.com>
2023-10-17 11:53:01 +0200

    fix

--- models/inscription-files/scoresflappy.php
+++ models/inscription-files/scoresflappy.php
@@ -3,16 +3,13 @@
 //include 'loginDB.php';
 require_once '../../loginDB.php';
 
-
+/*
 //coonection mysqli
-try {
-    $conn = mysqli_connect($servername, $username, $password, $dbname, $db_port);
-}
-catch (Exception $e){
-    $error = $e->getMessage();
-    echo $error;
-}
-echo $conn->host_info . "\n";
+
+    $conn = mysqli_connect($servername, $username, $password, $dbname);
+
+
+//echo $conn->host_info . "\n";
 // requete des score floppy
 $result = mysqli_query($conn,'SELECT Comptes.Name AS Pseudo, ScoreNb FROM Scores
 INNER JOIN Comptes ON Scores.UID=Comptes.UID
@@ -25,5 +22,19 @@ while ($row = mysqli_fetch_assoc($result)){
 }
 // returning in form of json
 echo json_encode($data);
-//echo('5');
+*/
+
+$conn=mysqli_connect($servername,$username,$password,$dbname);
+// requete des score floppy
+$result = mysqli_query($conn,'SELECT Comptes.Name AS Pseudo, ScoreNb FROM Scores
+INNER JOIN Comptes ON Scores.UID=Comptes.UID
+INNER JOIN Jeu ON Scores.IDGAME = Jeu.IDGAME
+WHERE NomJeu = "Flappy Bird" ORDER BY ScoreNb DESC;');
+
+$data = array();
+while ($row = mysqli_fetch_assoc($result)){
+    $data[] = $row;
+}
+// returning in form of json
+echo json_encode($data);
 ?>
